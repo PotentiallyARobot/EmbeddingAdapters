8681e38fa0139dc1c7e79526dda03d2ec4adc5a6
aliceauth <alice.ai.app@gmail.com>
2026-04-12 09:43:10 -0400

    fixes js spillover in python examples

--- js/api.js
+++ js/api.js
@@ -77,26 +77,26 @@ function switchCodeTab(gid, idx, btn) {
 function highlightCode(code, lang) {
   let h = escHtml(code);
   if (lang === "python" || lang === "py") {
-    h = h.replace(/(#.*)/g, '<span style="color:#6b7280;">$1</span>');
-    h = h.replace(/\b(import|from|as|def|class|return|if|else|elif|for|in|while|with|try|except|raise|not|and|or|is|None|True|False|print|async|await)\b/g, '<span style="color:#c084fc;">$1</span>');
-    h = h.replace(/(["'])(?:(?!\1).)*?\1/g, '<span style="color:#34d399;">$&</span>');
-    h = h.replace(/\b(\d+\.?\d*)\b/g, '<span style="color:#fbbf24;">$1</span>');
-    h = h.replace(/\b(np|requests|base64|torch|os|time|json)\b/g, '<span style="color:#60a5fa;">$1</span>');
+    h = h.replace(/(#.*)/g, '<span class=hl-cm>$1</span>');
+    h = h.replace(/(["'])(?:(?!\1).)*?\1/g, '<span class=hl-st>$&</span>');
+    h = h.replace(/\b(import|from|as|def|class|return|if|else|elif|for|in|while|with|try|except|raise|not|and|or|is|None|True|False|print|async|await)\b/g, '<span class=hl-kw>$1</span>');
+    h = h.replace(/\b(\d+\.?\d*)\b/g, '<span class=hl-nm>$1</span>');
+    h = h.replace(/\b(np|requests|base64|torch|os|time|json)\b/g, '<span class=hl-lb>$1</span>');
   } else if (lang === "bash" || lang === "sh") {
-    h = h.replace(/(#.*)/g, '<span style="color:#6b7280;">$1</span>');
-    h = h.replace(/(["'])(?:(?!\1).)*?\1/g, '<span style="color:#34d399;">$&</span>');
-    h = h.replace(/\b(curl|pip|git|cd|export|python|set)\b/g, '<span style="color:#c084fc;">$1</span>');
-    h = h.replace(/(--?\w[\w-]*)/g, '<span style="color:#60a5fa;">$1</span>');
+    h = h.replace(/(#.*)/g, '<span class=hl-cm>$1</span>');
+    h = h.replace(/(["'])(?:(?!\1).)*?\1/g, '<span class=hl-st>$&</span>');
+    h = h.replace(/\b(curl|pip|git|cd|export|python|set|embedding-adapters)\b/g, '<span class=hl-kw>$1</span>');
+    h = h.replace(/(--?\w[\w-]*)/g, '<span class=hl-lb>$1</span>');
   } else if (lang === "javascript" || lang === "js") {
-    h = h.replace(/(\/\/.*)/g, '<span style="color:#6b7280;">$1</span>');
-    h = h.replace(/\b(const|let|var|function|return|if|else|for|while|await|async|new|true|false|null|undefined)\b/g, '<span style="color:#c084fc;">$1</span>');
-    h = h.replace(/(["'`])(?:(?!\1).)*?\1/g, '<span style="color:#34d399;">$&</span>');
-    h = h.replace(/\b(\d+\.?\d*)\b/g, '<span style="color:#fbbf24;">$1</span>');
+    h = h.replace(/(\/\/.*)/g, '<span class=hl-cm>$1</span>');
+    h = h.replace(/(["'`])(?:(?!\1).)*?\1/g, '<span class=hl-st>$&</span>');
+    h = h.replace(/\b(const|let|var|function|return|if|else|for|while|await|async|new|true|false|null|undefined)\b/g, '<span class=hl-kw>$1</span>');
+    h = h.replace(/\b(\d+\.?\d*)\b/g, '<span class=hl-nm>$1</span>');
   } else if (lang === "json") {
-    h = h.replace(/(["'])(\w+)\1\s*:/g, '<span style="color:#60a5fa;">$&</span>');
-    h = h.replace(/:\s*(["'])(?:(?!\1).)*?\1/g, '<span style="color:#34d399;">$&</span>');
-    h = h.replace(/:\s*(\d+\.?\d*)/g, ': <span style="color:#fbbf24;">$1</span>');
-    h = h.replace(/\b(true|false|null)\b/g, '<span style="color:#c084fc;">$1</span>');
+    h = h.replace(/(["'])(\w+)\1\s*:/g, '<span class=hl-lb>$&</span>');
+    h = h.replace(/:\s*(["'])(?:(?!\1).)*?\1/g, '<span class=hl-st>$&</span>');
+    h = h.replace(/:\s*(\d+\.?\d*)/g, ': <span class=hl-nm>$1</span>');
+    h = h.replace(/\b(true|false|null)\b/g, '<span class=hl-kw>$1</span>');
   }
   return h;
 }
